3dbcebc0b53b4e15bed8ca44fa440250ab58bb74
unknown <hpharipriya@gmail.com>
2020-09-17 18:27:56 +0530

    initial data migrations

--- predict/admin.py
+++ predict/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Team, Prediction, Match
+from .models import Category, Team, Prediction, Match, MatchStatus, Venue
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Team)
 admin.site.register(Match)
+admin.site.register(MatchStatus)
+admin.site.register(Venue)
--- predict/models.py
+++ predict/models.py
@@ -2,35 +2,48 @@ from django.db import models
 
 # Create your models here.
 from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField()
-    logo = models.ImageField(blank=True, null=True)
+    logo = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+class MatchStatus(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    def __str__(self):
+        return self.name
+
+class Venue(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    image = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.name
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
-    status = models.IntegerField()
+    status = models.BooleanField(default=True)
     point = models.IntegerField()
     desc =  models.TextField()
     category =  models.ForeignKey(Category, on_delete=models.CASCADE)
-    logo = models.ImageField(blank=True, null=True)
+    logo = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.name
-
 
 class Match(models.Model):
     name = models.CharField(max_length=100)
     desc =  models.TextField()
+    venue =  models.ForeignKey(Venue,on_delete=models.CASCADE, default=1)
     match_date = models.DateField()
-    status = models.IntegerField()
+    status = models.ForeignKey(MatchStatus, on_delete=models.CASCADE)
     team_1 = models.ForeignKey(Team, on_delete=models.CASCADE)
-    #team_2 = models.ForeignKey(Team, on_delete=models.CASCADE)
     team_2 = models.ForeignKey(Team, related_name='%(class)s_team2',on_delete=models.CASCADE)
-    credit = models.IntegerField(default=100)
+    credit = models.IntegerField(default=10)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
@@ -47,7 +60,19 @@ class Prediction(models.Model):
     date_predicted = models.DateField()
     match_id = models.ForeignKey(Match, on_delete=models.CASCADE)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    status = models.IntegerField()
+    winning_team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    loosing_team = models.ForeignKey(Team, related_name='%(class)s_loosing',on_delete=models.CASCADE)
+    is_draw = models.BooleanField()
+    comments = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+class UserData(models.Model):
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    points = models.BigIntegerField(default=0)
+    credits = models.BigIntegerField(default=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
   
     
     
