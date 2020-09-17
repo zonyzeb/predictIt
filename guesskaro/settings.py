e248084d534421e03f7193ace5b2fa623becfaa4
unknown <hpharipriya@gmail.com>
2020-09-17 23:58:17 +0530

    mysql

--- guesskaro/settings.py
+++ guesskaro/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'guesskaro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'guess',
         'USER' : 'postgres',
         'PASSWORD': 'root',
@@ -150,5 +150,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
-
-django_heroku.settings(locals())
